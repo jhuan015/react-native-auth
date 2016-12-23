ac93b00ed67e22b9633d0396e5160402085de5d9
jhuan015 <jhuan015@ucr.edu>
2016-12-22 18:20:21 -0800

    Add input component and email section

--- src/app.js
+++ src/app.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View } from 'react-native'
 import { Header } from './components/common'
+import firebase from 'firebase'
+import LoginForm from './components/LoginForm'
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDWkQcs8oZeN9wRryQ2xLKMeoCAN3A3tPk',
+      authDomain: 'auth-react-native-ba781.firebaseapp.com',
+      databaseURL: 'https://auth-react-native-ba781.firebaseio.com',
+      storageBucket: 'auth-react-native-ba781.appspot.com',
+      messagingSenderId: '491299144224'
+    })
+  }
+
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <Text>An App!</Text>
+        <LoginForm />
       </View>
     )
   }
---
+++ src/components/LoginForm.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import { Button, Card, CardSection, Input } from './common'
+
+class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { email: '' };
+  }
+
+  render() {
+    return (
+      <Card>
+        <CardSection>
+          <Input
+            placeholder='user@gmail.com'
+            label='Email'
+            value={this.state.email}
+            onChangeText={email => this.setState({ email })}
+          />
+        </CardSection>
+        <CardSection></CardSection>
+        <CardSection>
+          <Button>
+            Log In
+          </Button>
+        </CardSection>
+      </Card>
+    )
+  }
+}
+
+export default LoginForm
